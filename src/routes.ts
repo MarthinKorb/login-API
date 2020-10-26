753038cc70019f293bff902ba1c4772cd286f4ba
Marthin Korb <marthinkorb@gmail.com>
2020-10-26 01:13:26 -0300

    Excluding ensureAuthenticated from delete users route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
+
 import SessionsController from './controllers/SessionsController';
 import UsersController from './controllers/UsersController';
+
 import ensureAuthenticated from './middlewares/ensureAuthenticated';
 
 import swaggerConfigs from './swagger';
@@ -15,7 +17,7 @@ routes.get('/api-docs', swaggerUi.setup(swaggerConfigs));
 routes.get('/users', UsersController.index);
 routes.post('/users', UsersController.create);
 routes.put('/users/:id', ensureAuthenticated, UsersController.edit);
-routes.delete('/users/:id', ensureAuthenticated, UsersController.delete);
+routes.delete('/users/:id', UsersController.delete);
 
 routes.post('/sessions', SessionsController.create);
 
