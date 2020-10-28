3a7a3b6d3569d2d15da2543c2deb5b82c9daf850
Marthin Korb <marthinkorb@gmail.com>
2020-10-28 00:34:27 -0300

    Finishing CreateUserService

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -10,18 +10,25 @@ interface RequestDTO {
     name: string;
     email: string;
     password: string;
+    passwordConfirmation: string;
 }
 
 export default class CreateUserService {
-    public async execute({ name, email, password }: RequestDTO): Promise<User> {
+    public async execute({
+        name,
+        email,
+        password,
+        passwordConfirmation,
+    }: RequestDTO): Promise<User> {
         const schema = Yup.object().shape({
             name: Yup.string().required(),
             email: Yup.string().required(),
             password: Yup.string().required(),
+            passwordConfirmation: Yup.string().required(),
         });
 
         await schema.validate(
-            { name, email, password },
+            { name, email, password, passwordConfirmation },
             {
                 abortEarly: false,
             },
@@ -32,6 +39,10 @@ export default class CreateUserService {
         const checkUserExists = await usersRepository.findOne({
             where: { email },
         });
+
+        if (password !== passwordConfirmation) {
+            throw Error('Password and password confirmation does not match');
+        }
 
         const hashedPassword = await hash(password, 8);
 
